82e3174302512b83cb61b70f97aed44887d8c0b5
Adarshkrishnan <adarshkrishnanpalayil@gmail.com>
2025-11-25 09:25:05 +0530

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: "standalone", // or just remove `export` if using `next export`
 };
 
 module.exports = nextConfig;
